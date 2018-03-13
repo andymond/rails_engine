50d1186468372661e19035b4a5adce36ad74139e
slimecog <wlcjohnson@gmail.com>
2018-03-12 19:08:27 -0600

    adds controllers for transaction, customer with index, show, tested

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :transactions, only: [:index, :show]
       resources :merchants, only: [:index, :show]
+      resources :customers, only: [:index, :show]
     end
   end
 end
