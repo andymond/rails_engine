24cdbb1978bd3684ce30e5851fe35f06e0b8fc1e
Andrew Dymond <andy.dymond@gmail.com>
2018-03-13 13:14:18 -0600

    shows a random item

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :items do
+        get "/random", to: "random#show"
         get "/find", to: "search#show"
         get "/find_all", to: "search#index"
       end
