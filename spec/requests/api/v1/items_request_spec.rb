0168c86c00c33b8a2269a849e6a90570fcf40ba9
Andrew Dymond <andy.dymond@gmail.com>
2018-03-13 15:26:08 -0600

    Add price convertion to strong params for items and invoice_items controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -37,6 +37,18 @@ describe "Items API" do
     expect(response_item["id"]).to eq(item.id)
   end
 
+  it "can find item by unit_price" do
+    item = create(:item, unit_price: 1000)
+    
+    get "/api/v1/items/find?unit_price=10.00"
+
+    expect(response).to be_success
+
+    response_item = JSON.parse(response.body)
+
+    expect(response_item["id"]).to eq(item.id)
+  end
+
   it "can find all items with matching attributes" do
     item = create(:item, name: "gold")
     item_2 = create(:item, name: "gold")
