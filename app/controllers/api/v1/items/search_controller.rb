5f407777faecd75413b5b92e9eb8623670bdaa1e
Andrew Dymond <andy.dymond@gmail.com>
2018-03-12 20:11:24 -0600

    Can find item by attribute spec passing

---
+++ app/controllers/api/v1/items/search_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::Items::SearchController < ApplicationController
+
+  def show
+    render json: Item.find_by(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+
+end
