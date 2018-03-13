46b786e9a49b76d6eac6cfbc9ffe079fb92dd293
Andrew Dymond <andy.dymond@gmail.com>
2018-03-12 20:23:56 -0600

    Can find all items w matching attributes spec passing

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index, :show]
       namespace :items do
         get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
       end
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
