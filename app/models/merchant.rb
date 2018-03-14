490a7ff080b5c8c9c7e54d30bc7a2f296704d71d
Andrew Dymond <andy.dymond@gmail.com>
2018-03-14 15:09:47 -0600

    Fix revenue method on merchant model, specs & spec harness passing for top merchants by revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,12 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
-  has_many :invoice_items, through: :invoices
-  has_many :transactions, through: :invoices
 
   def self.revenue
     select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
-      .joins(:invoice_items, :transactions)
+      .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.unscoped.successful)
       .group(:id)
       .order("revenue DESC")
