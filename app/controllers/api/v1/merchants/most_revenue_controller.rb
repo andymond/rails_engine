335b9645be360b916e2baf13d011369f27da9001
Andrew Dymond <andy.dymond@gmail.com>
2018-03-14 15:25:31 -0600

    Rename revenue controller to most_revenue controller

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Merchants::RevenueController < ApplicationController
+class Api::V1::Merchants::MostRevenueController < ApplicationController
 
   def index
     render json: Merchant.revenue.limit(revenue_params[:quantity])
