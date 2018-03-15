a0c984c2d7a4b11cc5b99757e34d4ea805156c1e
slimecog <wlcjohnson@gmail.com>
2018-03-15 15:29:12 -0600

    add to customer favorite item test

--- spec/requests/api/v1/customers/favorite_merchant_spec.rb
+++ spec/requests/api/v1/customers/favorite_merchant_spec.rb
@@ -6,13 +6,18 @@ describe "customers favorite merchant controller" do
     merchant1 = create(:merchant)
     merchant2 = create(:merchant)
     invoice1 = create(:invoice, customer: customer1, merchant: merchant1)
-    invoice1 = create(:invoice, customer: customer1, merchant: merchant2)
-    invoice1 = create(:invoice, customer: customer1, merchant: merchant1)
+    invoice2 = create(:invoice, customer: customer1, merchant: merchant2)
+    invoice3 = create(:invoice, customer: customer1, merchant: merchant1)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction1 = create(:transaction, invoice: invoice2)
+    transaction1 = create(:transaction, invoice: invoice3)
+
 
     get "/api/v1/customers/#{customer1.id}/favorite_merchant"
 
     results = JSON.parse(response.body)
 
     expect(response).to be_success
+    expect(results["name"]).to eq(merchant1.name)
   end
 end
