ef1c14d9edc60bbbf72e7abd40d7dc06b8175ce3
Andrew Dymond <andy.dymond@gmail.com>
2018-03-15 15:52:25 -0600

    Add merchants/:id/favorite_customer to routes, add model layer relationships to merchant & customer models, and add most_transactions(merchant_id) class method to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,15 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
+  has_many :merchants, through: :invoices
+
+  def self.most_transactions(merchant_id)
+    unscoped.select("customers.*, COUNT(transactions.result) AS transaction_count")
+      .joins(invoices: [:merchant, :transactions])
+      .merge(Transaction.unscoped.successful)
+      .where("merchants.id = ?", merchant_id)
+      .group(:id)
+      .order("transaction_count DESC")
+      .limit(1)
+  end
 end
