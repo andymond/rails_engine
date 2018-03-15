e3f01dc1ded84613945b5abaad2024555fe3d6e1
Andrew Dymond <andy.dymond@gmail.com>
2018-03-15 16:35:55 -0600

    Reformat json to return hash instead of array for spec

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
 
   def show
-    render json: Customer.most_transactions(params[:id])
+    render json: Customer.most_transactions(params[:id]).first
   end
 
 end
