7e8df747f7c906f5ce2eea11567e9b954feed32d
slimecog <wlcjohnson@gmail.com>
2018-03-15 15:55:36 -0600

    add merchant revenue for single merchant

---
+++ spec/requests/api/v1/merchants/revenue_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "merchant revenue controller" do
+  it "returns the total revenue for that merchant across successful transactions" do
+    merchant1 = create(:merchant)
+    invoice1 = create(:invoice, merchant: merchant1)
+    invoice2 = create(:invoice, merchant: merchant1)
+    invoice_item1 = create(:invoice_item, invoice: invoice1)
+    invoice_item2 = create(:invoice_item, invoice: invoice2)
+    invoice_item3 = create(:invoice_item, invoice: invoice1)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction2 = create(:transaction, invoice: invoice2)
+
+    get "/api/v1/merchants/#{merchant1.id}/revenue"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(results).to eq(Merchant.revenue.find(merchant1.id).revenue)
+  end
+end
