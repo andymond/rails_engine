4a88ed6b7da713210e09729dcf5097a289f9bed5
Andrew Dymond <andy.dymond@gmail.com>
2018-03-14 19:22:01 -0600

    Add default_scopes to order by id for all models

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,5 @@
 class InvoiceItem < ApplicationRecord
+  default_scope {order(:id)}
   scope :random, -> {order('random()').limit(1)}
   belongs_to :invoice, optional: true
   belongs_to :item, optional: true
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,5 @@
 class Merchant < ApplicationRecord
+  default_scope {order(:id)}
   has_many :items
   has_many :invoices
 
